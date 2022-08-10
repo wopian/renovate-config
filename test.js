8c22e3f95c8452a8ff17f8cc9f5bb22d366b6708
James Harris <wopian@wopian.me>
2022-08-11 00:47:41 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,31 +1,16 @@
 const { migrateAndValidate } = require('renovate/dist/config/migrate-validate')
+const config = require('./default.json')
 
 describe('@wopian/renovate-config', () => {
-  let pkg, config
-
-  beforeEach(() => {
-    pkg = require('./package.json')
-    config = pkg['renovate-config']
-  })
-
-  function checkConfig(name) {
-    describe(name, () => {
-      it('exists', () => {
-        expect(typeof config[name]).toBe('object')
-      })
-
-      it('is a valid config', async () => {
-        expect.assertions(2)
-        const { errors, warnings } = await migrateAndValidate({}, config[name])
-        expect(errors).toEqual([])
-        expect(warnings).toEqual([])
-      })
-    })
-  }
-
   it('has a renovate config', () => {
+    expect.assertions(1)
     expect(typeof config).toBe('object')
   })
 
-  checkConfig('default')
+  it('is a valid config', async () => {
+    expect.assertions(2)
+    const { errors, warnings } = await migrateAndValidate({}, config)
+    expect(errors).toEqual([])
+    expect(warnings).toEqual([])
+  })
 })
